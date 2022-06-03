7bc5ef730cb02b444bd4fe412cfd2a0db9934506
Jatin141 <106056823+Jatin141@users.noreply.github.com>
2022-06-03 11:23:21 +0530

    Update AppDelegate.swift

--- iOS/AppDelegate.swift
+++ iOS/AppDelegate.swift
@@ -1,1 +1,32 @@
 
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+    
+    var myOrientation: UIInterfaceOrientationMask = .portrait
+    
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+      GeneratedPluginRegistrant.register(with: self)
+
+                  weak var registrar = self.registrar(forPlugin: "ima_sdk")
+
+                  let factory = NativeViewFactory(messenger: registrar!.messenger())
+                  registrar!.register(
+                      factory,
+                      withId: "NativeUI")
+              
+          return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+        
+  }
+    
+    override func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+            return myOrientation
+        }
+    
+}
