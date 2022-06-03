82183784a63c522746526e062f77dc0d0f746ca8
Jatin141 <106056823+Jatin141@users.noreply.github.com>
2022-06-03 11:30:16 +0530

    Create MainActivity.kt

---
+++ Android/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.gymfans
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        flutterEngine
+            .platformViewsController
+            .registry
+            .registerViewFactory("NativeUI", NativeViewFactory(flutterEngine.dartExecutor.binaryMessenger,this))
+
+    }
+}
